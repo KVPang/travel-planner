9a41dae098b5a69aff7eb79fc6aef1225dfe4c84
Kristina Pang <kvypang@gmail.com>
2022-05-04 11:05:38 -0700

    traveler model

---
+++ models/traveler.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Traveler extends Model {}
+
+Traveler.init({
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    }
+}, {
+    sequelize,
+    timestamps: false
+});
+
+Model.exports = Traveler; 
